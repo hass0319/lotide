d31e4e7ba0220d0f9b4aed938c41384fe4f1bd61
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-08 02:47:05 +0000

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("🛑🛑🛑Assertion Passed: " + actual + " === " + expected);
+  } else {
+    return console.log("✅✅✅Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("lighthouse Labs","Bootcamp");
+assertEqual("lighthouse Labs","lighthouse Labs");
+assertEqual(1,1);
+assertEqual(1,2);
